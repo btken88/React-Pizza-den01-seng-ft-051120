1bc1317de9ef04ca608ed1cb6e02655d564be9db
Bryce Kennedy <btken88@gmail.com>
2020-07-26 15:26:26 -0600

    Create controlled pizza form inputs using hooks with state held in the main app.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useState } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
-      </Fragment>
-    );
+function App() {
+  const [topping, setTopping] = useState("")
+  const [size, setSize] = useState("Small")
+  const [vegetarian, setVegetarian] = useState(null)
+  const [nonVegetarian, setNonVegetarian] = useState(null)
+
+  function changeVegetarianState(event) {
+    setVegetarian(event.target.value)
+    setNonVegetarian(null)
   }
+
+  function changeNonVegetarianState(event) {
+    setNonVegetarian(event.target.value)
+    setVegetarian(null)
+  }
+
+  const pizzaForm = [
+    topping,
+    setTopping,
+    size,
+    setSize,
+    vegetarian,
+    nonVegetarian,
+    changeVegetarianState,
+    changeNonVegetarianState
+  ]
+
+  return (
+    <Fragment>
+      <Header />
+      <PizzaForm pizzaForm={[...pizzaForm]} />
+      <PizzaList />
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,39 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
 
-const PizzaForm = () => {
-  return(
-      <div className="form-row">
-        <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
-        </div>
-        <div className="col">
-          <select value={null} className="form-control">
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
-          </select>
-        </div>
-        <div className="col">
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
-            <label className="form-check-label">
-              Vegetarian
+const PizzaForm = ({ pizzaForm }) => {
+  const [topping,
+    setTopping,
+    size,
+    setSize,
+    vegetarian,
+    nonVegetarian,
+    changeVegetarianState,
+    changeNonVegetarianState
+  ] = pizzaForm
+
+  return (
+    <div className="form-row">
+      <div className="col-5">
+        <input type="text"
+          className="form-control"
+          placeholder="Pizza Topping"
+          value={topping}
+          onChange={e => setTopping(e.target.value)} />
+      </div>
+      <div className="col">
+        <select value={size}
+          className="form-control"
+          onChange={e => setSize(e.target.value)}>
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+        </select>
+      </div>
+      <div className="col">
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            value="Vegetarian"
+            checked={vegetarian}
+            onClick={changeVegetarianState} />
+          <label className="form-check-label">
+            Vegetarian
             </label>
-          </div>
-          <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
-            <label className="form-check-label">
-              Not Vegetarian
-            </label>
-          </div>
         </div>
-        <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio" value="Not Vegetarian"
+            checked={nonVegetarian}
+            onClick={changeNonVegetarianState} />
+          <label className="form-check-label">
+            Not Vegetarian
+            </label>
         </div>
       </div>
+      <div className="col">
+        <button type="submit" className="btn btn-success" onClick={() => console.log('State: ', topping, size, vegetarian, nonVegetarian)}>Submit</button>
+      </div>
+    </div>
 
   )
 }
